e961e258a45dc9fd19dc08a11e183823e35c5cd9
Haen <77063375+haen-su@users.noreply.github.com>
2023-01-16 03:00:50 +0900

    Revert "도메인 구현"

--- src/main/java/WOWMATE/repository/SchoolRepository.java
+++
@@ -1,8 +0,0 @@
-package WOWMATE.repository;
-
-import WOWMATE.domain.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRepository extends JpaRepository<School, Long> {
-}
-
