6da64f4767ace733ff2d7a6f8ebf29c815ed396c
Chan-Heee <cksgml019@naver.com>
2023-02-01 19:15:46 +0900

    feat:post-api 수정

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,15 +1,4 @@
-insert into User (user_id, age, birth, email, gender, password, phone_number, univ) values (1, 23, "2000-03-16", "chlwogns316", "M", "1234", "01035810915", "홍익대학교");
-
 insert into University (univ_id, name, email_domain) values (1, '홍익대', 'g.hongik.ac.kr');
 insert into University (univ_id, name, email_domain) values (2, '숙명여대', 'g.sookmyung.ac.kr');
 insert into University (univ_id, name, email_domain) values (3, '명지대', 'g.mju.ac.kr');
 insert into University (univ_id, name, email_domain) values (4, '성신여대', 'g.sungshin.ac.kr');
-
-insert into Post (post_id, context, like_number, tag1, tag2, tag3, tag4, tag5, title, category_name, user_id) values (10000, "게시글1-1", 0, "태그1", "태그2", "태그3", "태그4", "태그5", "게시글 1번", "a", 1);
-insert into Post (post_id, context, like_number, tag1, tag2, tag3, tag4, tag5, title, category_name, user_id) values (20000, "게시글2-2", 0, "태그1", "태그2", "태그3", "태그4", "태그5", "게시글 2번", "a", 1);
-insert into Post (post_id, context, like_number, tag1, tag2, tag3, tag4, tag5, title, category_name, user_id) values (30000, "게시글3-3", 0, "태그1", "태그2", "태그3", "태그4", "태그5", "게시글 3번", "b", 1);
-insert into Post (post_id, context, like_number, tag1, tag2, tag3, tag4, tag5, title, category_name, user_id) values (40000, "게시글4-4", 0, "태그1", "태그2", "태그3", "태그4", "태그5", "게시글 4번", "b", 1);
-insert into Post (post_id, context, like_number, tag1, tag2, tag3, tag4, tag5, title, category_name, user_id) values (50000, "게시글5-1", 0, "태그1", "태그2", "태그3", "태그4", "태그5", "게시글 5번", "c", 1);
-insert into Post (post_id, context, like_number, tag1, tag2, tag3, tag4, tag5, title, category_name, user_id) values (60000, "게시글6-2", 0, "태그1", "태그2", "태그3", "태그4", "태그5", "게시글 6번", "c", 1);
-insert into Post (post_id, context, like_number, tag1, tag2, tag3, tag4, tag5, title, category_name, user_id) values (70000, "게시글7-3", 0, "태그1", "태그2", "태그3", "태그4", "태그5", "게시글 7번", "d", 1);
-insert into Post (post_id, context, like_number, tag1, tag2, tag3, tag4, tag5, title, category_name, user_id) values (80000, "게시글8-4", 0, "태그1", "태그2", "태그3", "태그4", "태그5", "게시글 8번", "d", 1);
