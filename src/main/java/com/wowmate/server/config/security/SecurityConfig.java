187b7b8e414cfa0eb8fe81f35203159b226d5c62
ImTakGyun <gyun1712@gmail.com>
2023-02-02 22:54:35 +0900

    이메일 인증 구현 수정본

--- src/main/java/com/wowmate/server/config/security/SecurityConfig.java
+++ src/main/java/com/wowmate/server/config/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests() // 리퀘스트에 대한 사용권한 체크
                 .antMatchers("/sign-api/sign-in", "/sign-api/sign-up", "/sign-api/exception",
-                        "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**")
+                        "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/emailConfirm", "/sign-api/univ")
                 .permitAll() // 가입 및 로그인 주소는 허용
                 .antMatchers("/admin-api/**")
                 .hasRole("ADMIN")
--- src/main/java/com/wowmate/server/user/service/impl/EmailServiceImpl.java
+++ src/main/java/com/wowmate/server/user/service/impl/EmailServiceImpl.java
@@ -2,7 +2,6 @@ package com.wowmate.server.user.service.impl;
 
 import com.wowmate.server.user.service.EmailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -16,35 +15,48 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
-    @Autowired
-    JavaMailSender emailSender;
-
-    public static final String ePw = createKey();
+    private final JavaMailSender emailSender;
+    public static String ePw;
 
     private MimeMessage createMessage(String to)throws Exception{
+
+        ePw = createKey();
+
         System.out.println("보내는 대상 : "+ to);
         System.out.println("인증 번호 : "+ePw);
-        MimeMessage  message = emailSender.createMimeMessage();
 
+        MimeMessage  message = emailSender.createMimeMessage();
         message.addRecipients(Message.RecipientType.TO, to);//보내는 대상
-        message.setSubject("이메일 인증 테스트");//제목
+        message.setSubject("[WOWMATE] 인증번호 발송");//제목
 
         String msgg="";
         msgg+= "<div style='margin:20px;'>";
-        msgg+= "<h1> 안녕하세요 WOWMATE입니다. </h1>";
-        msgg+= "<br>";
-        msgg+= "<p>아래 코드를 복사해 입력해주세요<p>";
-        msgg+= "<br>";
-        msgg+= "<p>감사합니다.<p>";
-        msgg+= "<br>";
-        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg+= "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
-        msgg+= "<div style='font-size:130%'>";
-        msgg+= "CODE : <strong>";
-        msgg+= ePw+"</strong><div><br/> ";
-        msgg+= "</div>";
+        msgg+= "<table border='0' cellpadding='0' cellspacing='0' align='center' style='width:100%;max-width:630px;margin-left:auto;margin-right:auto;letter-spacing:-1px'>";
+        msgg+= "<tbody>";
+        msgg+= "<tr><td style='font-size:44px;line-height:48px;font-weight:bold;padding-bottom:60px;text-align:left;letter-spacing:-1px;'>";
+        msgg+= "<span>이메일 인증번호 안내</span>";
+        msgg+= "</td></tr>";
+        msgg+= "<tr><td style='font-size:28px;font-weight:bold;color:purple;'>";
+        msgg+= "<span>WOWMATE</span>";
+        msgg+= "</td></tr>";
+        msgg+= "<tr><td style='padding-bottom:50px;font-size:14px;line-height:22px;text-align:left;letter-spacing:-1px;'>";
+        msgg+= "<p style='margin:0;padding:0'>본 메일은 WOWMATE 회원가입을 위한 이메일 인증입니다.</p>";
+        msgg+= "<p style='margin:0;padding:0'>아래의 [이메일 인증번호]를 입력하여 본인확인을 해주시기 바랍니다.</p>";
+        msgg+= "</td></tr>";
+        msgg+= "<tr><td style='padding-bottom:50px'>";
+        msgg+=    "<table border='0' cellpadding='0' cellspacing='0' align='center' style='width:100%'>";
+        msgg+=        "<tbody>";
+        msgg+=            "<tr><td style='padding:50px 30px;border:1px solid #eeeeee;background:#fbfbfb;text-align:center'>";
+        msgg+=                "<p style='margin:0;padding:0;font-size:18px;font-weight:bold;letter-spacing:-1px;'>" + ePw + "</p>";
+        msgg+=            "</td></tr>";
+        msgg+=    "</tbody></table>";
+        msgg+= "</td></tr>";
+        msgg+= "<tr><td style='font-size:14px;line-height:22px;text-align:left;letter-spacing:-1px;'>";
+        msgg+= "<p style='margin:0;padding:0'>감사합니다.<br>WOWMATE 드림</p>";
+        msgg+= "</td></tr>";
+        msgg+= "</tbody></table>";
         message.setText(msgg, "utf-8", "html");//내용
-        message.setFrom(new InternetAddress("gyun1712@gmail.com","gyun1712"));//보내는 사람
+        message.setFrom(new InternetAddress("gyun1712@gmail.com","WOWMATE"));//보내는 사람
 
         return message;
     }
