397b729e90b6e9e2ce8c398ac94b3abf476290d7
TakGyun <gyun1712@gmail.com>
2023-02-13 23:16:07 +0900

    Update S3Config.java

--- src/main/java/com/wowmate/server/config/S3/S3Config.java
+++ src/main/java/com/wowmate/server/config/S3/S3Config.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application-prod.yml")
+//@PropertySource("classpath:application-prod.yml")
 public class S3Config {
     @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
